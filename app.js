aa2ccdf81e2876074cc0441e0ed40dd406a6b569
challenget <466023746@qq.com>
2017-03-01 09:58:33 +0800

    '2'

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
  * Created by loutao on 2017/2/24.
  */
 
+var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
 var privateKey  = fs.readFileSync('./challenget.win.key', 'utf8');
 var certificate = fs.readFileSync('./2_challenget.win.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
+
+var httpServer = http.createServer(function (req, res) {
+    res.statusCode = 301;
+    res.setHeader('Location', 'https://' + req.headers.host + req.url);
+    res.end();
+});
+var PORT = 80;
+
+httpServer.listen(PORT, function () {
+    console.log('HTTP Server is running on port %s', PORT);
+});
 
 var httpsServer = https.createServer(credentials, function (req, res) {
     res.writeHead(200);
@@ -16,5 +28,5 @@ var httpsServer = https.createServer(credentials, function (req, res) {
 var SSLPORT = 443;
 
 httpsServer.listen(SSLPORT, function() {
-    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+    console.log('HTTPS Server is running on port %s', SSLPORT);
 });
